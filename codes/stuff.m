d32f9bdb00c07a82e43eeb7c2dc5b46f7b291e61
matteo <teo_romi@hotmail.it>
2020-10-27 01:23:10 +0100

    another example

--- codes/stuff.m
+++ codes/stuff.m
@@ -5,26 +5,6 @@ clc, clear, close all
 
 
 Som = 0.00059;
-SM = 0.0013;
-Skep = diag([Sa Se Si SOM Som SM]);
-
-N = 100000;
-kep = mvnrnd(kep0,Skep,N);
-
-% figure
-% hold on
-% plot(kep(:,1),kep(:,2),'.')
-
-rp = kep(:,1).*(1-kep(:,2));
-t0 = cspice_str2et(date); %TBD
-elts = [rp,kep(:,2:6),t0*ones(N,1),muSun*ones(N,1)];
-
-x0 = cspice_conics(elts',t0*ones(1,N)); x0=x0';
-
-% figure
-% axis equal
-% plot3(x0(:,1),x0(:,2),x0(:,3),'.')
-% grid on
 
 Sx = cov(x0)
 
